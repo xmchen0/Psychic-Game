5a7d496360b7c934dcfd4cced75593a0c2f7b840
Kathy Chen <kathychen.social@gmail.com>
2019-02-02 01:22:34 -0500

    uploading finishing touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,55 @@
+/* UofT Bootcamp 2019 - Psychic Game */
+
 /// VARIABLES
 // define variables and data used at a global level
 var winCount = 0;          
 var lossCount = 0;                 
-var guessesRemaining = 10;
+var guessesRemaining = 9;
 var guessedLetters = [];
-var randomLetter = randomLetter;
-var resetButton;
-
-// set up array for computer to choose from
 var alphabet =
     ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
     ,"r","s","t","u","v","w","x","y","z"];
 
 
 /// FUNCTIONS
-//1. set up the computer to pick a random indexed value from array
+//1. assign local variable for computer pick a random value from letters array
 randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
     computerChoice = alphabet[randomLetter];
-    console.log(computerChoice);
+    
 
 //2. add a listener to capture the players input
 document.onkeyup = function(event) {
     var userGuess = event.key;
+    var keyPress = (String.fromCharCode(event.keyCode)).toLowerCase();
 
-    //3. make sure the user selects a value a-z
-    var keyPress = /[a-z]/;
+//3. make sure the user selects a value a-z
+var keyPress = /[a-z]/;
     if (!keyPress.test(userGuess)) {
-      alert("Error: please enter only letters");
+      alert("Ops! Please enter only letters");
       return false;
     }
 
-    //4. if player guesses correctly, win by 1 and clears used letters array
-    if (userGuess == randomLetter) {
+//4. if player guesses correctly, win by 1 and clear used letters array
+if (userGuess === randomLetter) {
     alert("Congratulations! You got it right!");
     winCount++;
     guessedLetters = [];
-    guessesRemaining = 11;
+    guessesRemaining = 9;
     }
 
-    //5. if player guesses incorrectly, reduce guessesRemaining by 1
-    randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(randomLetter);
-
-    if (userGuess !== randomLetter) {
-        guessesRemaining--;
+//5. if player guesses incorrectly, reduce guessesRemaining by 1 and add new item to the array
+if (userGuess != randomLetter) {
+    guessesRemaining--;
+    guessedLetters.push(userGuess);
     }
     
-    //6. when remaining guesses equals zero, lose by 1, reset to zero and used arrays to equal zero
-    if (guessesRemaining == 0) {
-        alert("Oh no! Computer Won!");
-        lossCount++;
-        guessedLetters = [];
-        guessesRemaining = 10;
+//6. if guessesRemaining is less than 1, reset to zero and clear used letters array
+if (guessesRemaining < 1) {
+    alert("Oh no! Computer Won!");
+    lossCount++;
+    guessedLetters = [];
+    guessesRemaining = 9;
     }
-
-    //7. prevent a letter being selected twice 
-    if (guessedLetters.indexOf(userGuess) >= 0) {
-        } else {
-        guessedLetters.push(userGuess);
-    }
-
 
 // storing HTML elements as objects
 document.getElementById("winCount").innerHTML = winCount;
